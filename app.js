e191e701b92f3b20fd8ede23bff7a07803a7e5dd
Prasanna V <you@example.com>
2023-12-14 16:06:51 +0530

    coded

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const audio = new Audio('path/to/your/audiofile.mp3');
 
-const playButton = document.getElementById('playButton');
-
-playButton.addEventListener('click', function () {
-  audio.play();
-});
 let clickingThePlayBtn = document.querySelector('.playbtn');
 clickingThePlayBtn.addEventListener('click', function () {
   window.location.href = 'nickname.html';
